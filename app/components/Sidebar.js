e25e60060664eff4fb9da90b8860b9f3230c539c
Daniel Arrizza <daniel.arrizza@appdynamics.com>
2018-08-21 14:52:34 -0400

    move default queries to its own file

--- app/components/Sidebar.js
+++ app/components/Sidebar.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import defaultQueries from './defaultQueries'
 // import styles from './Home.css'
 
 export const SavedQuery = ({ title, onQuerySelect }) => (
@@ -12,53 +13,6 @@ export const SavedQuery = ({ title, onQuerySelect }) => (
   </div>
 )
 
-const savedQueries = [
-  {
-    title: 'All Applications',
-    query: 'SELECT application, health, art, cpm, epm FROM applications',
-  },
-  {
-    title: 'One Application',
-    query:
-      'SELECT application, health, art, cpm, epm FROM applications WHERE application = "MyApp"',
-  },
-  {
-    title: 'Some Applications',
-    query:
-      'SELECT application, health, art, cpm, epm FROM applications WHERE application REGEXP "MyApp1|MyApp2|MyApp3"',
-  },
-  {
-    title: 'All BTs in an App',
-    query:
-      'SELECT bt, health, art, cpm, epm FROM applications WHERE application = "MyApp"',
-  },
-  {
-    title: 'Some BTs in an App',
-    query:
-      'SELECT bt, health, art, cpm, epm FROM applications WHERE application = "MyApp" AND bt REGEXP "MyBT.*"',
-  },
-  {
-    title: 'Some BTs from different Apps',
-    query:
-      'SELECT bt, health, art, cpm, epm FROM applications WHERE application REGEXP "MyApp1|MyApp2|MyApp3 AND bt REGEXP "Login.*"',
-  },
-  {
-    title: 'All Tiers in an App',
-    query:
-      'SELECT tier, health, art, cpm, epm FROM applications WHERE application = "MyApp"',
-  },
-  {
-    title: 'All Nodes in an App',
-    query:
-      'SELECT node, health, art, cpm, epm FROM applications WHERE application = "MyApp"',
-  },
-  {
-    title: 'All SEs in an App',
-    query:
-      'SELECT se, art, cpm, epm FROM applications WHERE application = "MyApp"',
-  },
-]
-
 export default class Sidebar extends Component {
   selectQuery = (title, query) => {
     this.props.selectQuery({ title, query })
@@ -69,14 +23,13 @@ export default class Sidebar extends Component {
         <h4 className="saved-queries-title">Sample Queries</h4>
         <div className="queries-list">
           <div>
-            {savedQueries &&
-              savedQueries.map(({ title, query }) => (
-                <SavedQuery
-                  key={title}
-                  title={title}
-                  onQuerySelect={this.selectQuery.bind(null, title, query)}
-                />
-              ))}
+            {defaultQueries.map(({ title, query }) => (
+              <SavedQuery
+                key={title}
+                title={title}
+                onQuerySelect={this.selectQuery.bind(null, title, query)}
+              />
+            ))}
           </div>
         </div>
       </div>
---
+++ app/components/defaultQueries.js
@@ -0,0 +1,46 @@
+export default [
+  {
+    title: 'All Applications',
+    query: 'SELECT application, health, art, cpm, epm FROM applications',
+  },
+  {
+    title: 'One Application',
+    query:
+      'SELECT application, health, art, cpm, epm FROM applications WHERE application = "MyApp"',
+  },
+  {
+    title: 'Some Applications',
+    query:
+      'SELECT application, health, art, cpm, epm FROM applications WHERE application REGEXP "MyApp1|MyApp2|MyApp3"',
+  },
+  {
+    title: 'All BTs in an App',
+    query:
+      'SELECT bt, health, art, cpm, epm FROM applications WHERE application = "MyApp"',
+  },
+  {
+    title: 'Some BTs in an App',
+    query:
+      'SELECT bt, health, art, cpm, epm FROM applications WHERE application = "MyApp" AND bt REGEXP "MyBT.*"',
+  },
+  {
+    title: 'Some BTs from different Apps',
+    query:
+      'SELECT bt, health, art, cpm, epm FROM applications WHERE application REGEXP "MyApp1|MyApp2|MyApp3 AND bt REGEXP "Login.*"',
+  },
+  {
+    title: 'All Tiers in an App',
+    query:
+      'SELECT tier, health, art, cpm, epm FROM applications WHERE application = "MyApp"',
+  },
+  {
+    title: 'All Nodes in an App',
+    query:
+      'SELECT node, health, art, cpm, epm FROM applications WHERE application = "MyApp"',
+  },
+  {
+    title: 'All SEs in an App',
+    query:
+      'SELECT se, art, cpm, epm FROM applications WHERE application = "MyApp"',
+  },
+]
