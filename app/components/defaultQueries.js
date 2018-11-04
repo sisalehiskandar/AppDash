acdb073476cc916de23611faf6eb0c92989ed479
Daniel Arrizza <daniel.arrizza@appdynamics.com>
2018-11-04 09:43:02 -0500

    add more templates and make sidebar load them automatically

--- app/components/defaultQueries.js
+++ app/components/defaultQueries.js
@@ -1,4 +1,12 @@
+import templates from '../dashboardTemplates/templates'
+
 export default [
+  ...Object.keys(templates).map(key => ({
+    type: 'TEMPLATE',
+    title: key,
+    query:
+      'SELECT application FROM applications WHERE application REGEXP "MyApp1|MyApp2"',
+  })),
   {
     type: 'GRID',
     title: 'All Applications',
@@ -63,53 +71,5 @@ export default [
     title: 'Some SEs in different Apps',
     query:
       'SELECT se, art, cpm, epm FROM applications WHERE application REGEXP "MyApp" AND se REGEXP "Login.*"',
-  },
-  {
-    type: 'TEMPLATE',
-    title: 'Overall Application Health',
-    query:
-      'SELECT application FROM applications WHERE application REGEXP "MyApp1|MyApp2"',
-  },
-  {
-    type: 'TEMPLATE',
-    title: 'Application - Memory',
-    query:
-      'SELECT application FROM applications WHERE application REGEXP "MyApp1|MyApp2"',
-  },
-  {
-    type: 'TEMPLATE',
-    title: 'Application - OS',
-    query:
-      'SELECT application FROM applications WHERE application REGEXP "MyApp1|MyApp2"',
-  },
-  {
-    type: 'TEMPLATE',
-    title: 'Application - Performance',
-    query:
-      'SELECT application FROM applications WHERE application REGEXP "MyApp1|MyApp2"',
-  },
-  {
-    type: 'TEMPLATE',
-    title: 'Dev - Sample 1',
-    query:
-      'SELECT application FROM applications WHERE application REGEXP "MyApp1|MyApp2"',
-  },
-  {
-    type: 'TEMPLATE',
-    title: 'Ops - Sample 2',
-    query:
-      'SELECT application FROM applications WHERE application REGEXP "MyApp1|MyApp2"',
-  },
-  {
-    type: 'TEMPLATE',
-    title: 'Ops - Sample 3',
-    query:
-      'SELECT application FROM applications WHERE application REGEXP "MyApp1|MyApp2"',
-  },
-  {
-    type: 'TEMPLATE',
-    title: 'Ops - Sample 4',
-    query:
-      'SELECT application FROM applications WHERE application REGEXP "MyApp1|MyApp2"',
   },
 ]
